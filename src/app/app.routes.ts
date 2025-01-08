1e15fea206727027d424fce2ee32be8bef2e039d
himanshu-inexture <himanshup.inexture@gmail.com>
2025-01-08 16:19:25 +0530

    created register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     // {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'register', component: RegisterComponent}
 ];
