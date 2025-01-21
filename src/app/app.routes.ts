1ddd36066141677fb96cec1c55017493555ca72e
himanshu-inexture <himanshup.inexture@gmail.com>
2025-01-21 15:17:34 +0530

    created user homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { UserHomeComponent } from './user-home/user-home.component';
 
 export const routes: Routes = [
   // {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -9,4 +10,5 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  {path: 'userHome', component: UserHomeComponent}
 ];
---
+++ src/app/user-home/user-home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-home',
+  imports: [],
+  templateUrl: './user-home.component.html',
+  styleUrl: './user-home.component.css',
+})
+export class UserHomeComponent {}
