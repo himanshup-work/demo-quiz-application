d24aad57fc7564910506adb6c9ec1f6ac4b62f08
himanshup-work <himanshu.work2703@gmail.com>
2025-01-22 21:38:24 +0530

    made changes in create quiz page

--- src/app/create-quiz/create-quiz.component.ts
+++ src/app/create-quiz/create-quiz.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormArray,
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-quiz',
@@ -14,52 +8,51 @@ import {
   templateUrl: './create-quiz.component.html',
   styleUrl: './create-quiz.component.css',
 })
-export class CreateQuizComponent implements OnInit {
-  onSubmit() {
-    throw new Error('Method not implemented.');
-  }
-  quizForm: FormGroup;
+export class CreateQuizComponent implements OnInit{
+  quizForm!: FormGroup;
+  maxQuestions = 15; // Adjust as needed
 
-  constructor(private formBuilder: FormBuilder) {
-    this.quizForm = this.formBuilder.group({
-      quizTitle: ['', Validators.required],
-      questions: this.formBuilder.array([]),
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.quizForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      questions: this.fb.array([])
     });
   }
-  ngOnInit(): void {
-    // this.addQuestion();
-  }
 
-  get questions() {
+  get questionsFormArray(): FormArray {
     return this.quizForm.get('questions') as FormArray;
   }
 
-  addQuestion() {
-    const questionForm = this.formBuilder.group({
-      questionText: ['', Validators.required],
-      options: this.formBuilder.array([
-        this.formBuilder.control('', Validators.required),
-        this.formBuilder.control('', Validators.required),
-        this.formBuilder.control('', Validators.required),
-        this.formBuilder.control('', Validators.required),
-      ]),
-      correctOption: [0, Validators.required],
-    });
-
-    // // Add default 4 options
-    // const optionsArray = questionForm.get('options') as FormArray;
-    // for (let i = 0; i < 4; i++) {
-    //   optionsArray.push(this.formBuilder.control('', Validators.required));
-    // }
-
-    this.questions.push(questionForm);
+  getOptionsFormArray(index: number): FormArray {
+    return this.questionsFormArray.at(index).get('options') as FormArray;
   }
 
-  getOptionsFormArray(questionIndex: number): FormArray {
-    return this.questions.at(questionIndex).get('options') as FormArray;
+  addQuestion() {
+    if (this.questionsFormArray.length < this.maxQuestions) {
+      this.questionsFormArray.push(this.createQuestionFormGroup());
+    }
   }
 
   removeQuestion(index: number) {
-    this.questions.removeAt(index);
+    this.questionsFormArray.removeAt(index);
+  }
+
+  onSubmit() {
+    console.log(this.quizForm.value);
+  }
+
+  private createQuestionFormGroup(): FormGroup {
+    return this.fb.group({
+      text: ['', [Validators.required, Validators.minLength(5)]],
+      options: this.fb.array([
+        ['', Validators.required],
+        ['', Validators.required],
+        ['', Validators.required],
+        ['', Validators.required] // Adjust number of options as needed
+      ]),
+      correctOption: ['', Validators.required]
+    });
   }
 }
