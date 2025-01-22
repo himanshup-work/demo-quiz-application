bd33a6817a7a51ae7362f7b3689d589297223d36
himanshu-inexture <himanshup.inexture@gmail.com>
2025-01-22 18:25:16 +0530

    creating create quiz page

--- src/app/create-quiz/create-quiz.component.ts
+++ src/app/create-quiz/create-quiz.component.ts
@@ -1,78 +1,65 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder,  FormGroup, FormArray, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-quiz',
+  standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './create-quiz.component.html',
   styleUrl: './create-quiz.component.css',
 })
-export class CreateQuizComponent {
-  quizForm: FormGroup = new FormGroup({});
-  maxQuestions = 10;
-
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.initForm();
+export class CreateQuizComponent implements OnInit {
+  onSubmit() {
+    throw new Error('Method not implemented.');
   }
+  quizForm: FormGroup;
 
-  initForm() {
-    this.quizForm = this.fb.group({
-      title: ['', [Validators.required, Validators.minLength(3)]],
-      questions: this.fb.array([this.createQuestionGroup()])
+  constructor(private formBuilder: FormBuilder) {
+    this.quizForm = this.formBuilder.group({
+      quizTitle: ['', Validators.required],
+      questions: this.formBuilder.array([]),
     });
   }
+  ngOnInit(): void {
+    // this.addQuestion();
+  }
 
-  get questionsFormArray() {
+  get questions() {
     return this.quizForm.get('questions') as FormArray;
   }
 
-  createQuestionGroup(): FormGroup {
-    return this.fb.group({
-      text: ['', [Validators.required, Validators.minLength(5)]],
-      options: this.fb.array([
-        ['', [Validators.required]],
-        ['', [Validators.required]],
-        ['', [Validators.required]],
-        ['', [Validators.required]]
-      ])
+  addQuestion() {
+    const questionForm = this.formBuilder.group({
+      questionText: ['', Validators.required],
+      options: this.formBuilder.array([
+        this.formBuilder.control('', Validators.required),
+        this.formBuilder.control('', Validators.required),
+        this.formBuilder.control('', Validators.required),
+        this.formBuilder.control('', Validators.required),
+      ]),
+      correctOption: [0, Validators.required],
     });
+
+    // // Add default 4 options
+    // const optionsArray = questionForm.get('options') as FormArray;
+    // for (let i = 0; i < 4; i++) {
+    //   optionsArray.push(this.formBuilder.control('', Validators.required));
+    // }
+
+    this.questions.push(questionForm);
   }
 
   getOptionsFormArray(questionIndex: number): FormArray {
-    return this.questionsFormArray.at(questionIndex).get('options') as FormArray;
-  }
-
-  addQuestion() {
-    if (this.questionsFormArray.length < this.maxQuestions) {
-      this.questionsFormArray.push(this.createQuestionGroup());
-    }
+    return this.questions.at(questionIndex).get('options') as FormArray;
   }
 
   removeQuestion(index: number) {
-    if (this.questionsFormArray.length > 1) {
-      this.questionsFormArray.removeAt(index);
-    }
-  }
-
-  onSubmit() {
-    if (this.quizForm.valid) {
-      console.log('Quiz Data:', this.quizForm.value);
-      // TODO: Implement submission logic
-    } else {
-      this.markFormGroupTouched(this.quizForm);
-    }
-  }
-
-  // Helper method to mark all controls as touched to show validation errors
-  private markFormGroupTouched(formGroup: FormGroup | FormArray) {
-    Object.values(formGroup.controls).forEach(control => {
-      if (control instanceof FormGroup || control instanceof FormArray) {
-        this.markFormGroupTouched(control);
-      } else {
-        control.markAsTouched();
-      }
-    });
+    this.questions.removeAt(index);
   }
 }
